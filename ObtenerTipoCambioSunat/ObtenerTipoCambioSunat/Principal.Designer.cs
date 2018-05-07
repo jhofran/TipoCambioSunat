6394df0e6628de6bc686a9a4394741e5c94af968
jhofran <jhonyfrank@gmail.com>
2018-05-07 14:21:31 -0500

    Carga Inicial

---
+++ ObtenerTipoCambioSunat/ObtenerTipoCambioSunat/Principal.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace ObtenerTipoCambioSunat
+{
+    partial class Principal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.COL_FECHA = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.COL_COMPRA = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.COL_VENTA = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(754, 52);
+            this.panel1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.COL_FECHA,
+            this.COL_COMPRA,
+            this.COL_VENTA});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 52);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(282, 375);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(282, 52);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(472, 375);
+            this.webBrowser1.TabIndex = 2;
+            // 
+            // COL_FECHA
+            // 
+            this.COL_FECHA.HeaderText = "FECHA";
+            this.COL_FECHA.Name = "COL_FECHA";
+            this.COL_FECHA.ReadOnly = true;
+            this.COL_FECHA.Width = 75;
+            // 
+            // COL_COMPRA
+            // 
+            this.COL_COMPRA.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.COL_COMPRA.HeaderText = "COMPRA";
+            this.COL_COMPRA.Name = "COL_COMPRA";
+            this.COL_COMPRA.ReadOnly = true;
+            this.COL_COMPRA.Width = 70;
+            // 
+            // COL_VENTA
+            // 
+            this.COL_VENTA.HeaderText = "VENTA";
+            this.COL_VENTA.Name = "COL_VENTA";
+            this.COL_VENTA.ReadOnly = true;
+            this.COL_VENTA.Width = 70;
+            // 
+            // Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(754, 427);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Principal";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Principal_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn COL_FECHA;
+        private System.Windows.Forms.DataGridViewTextBoxColumn COL_COMPRA;
+        private System.Windows.Forms.DataGridViewTextBoxColumn COL_VENTA;
+    }
+}
+
